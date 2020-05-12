12f6373dc955a7d5d8d705b0ce6d3814e943bdc1
ofnullable <ofnullable@gmail.com>
2020-05-13 00:32:22 +0900

    :building_construction: Add server.ts and update tsconfig.json

---
+++ server/server.ts
@@ -0,0 +1,11 @@
+require('dotenv').config(); // load env first
+
+import app from './src/app';
+
+const port = Number(process.env.PORT) || 3030;
+
+app.listen(port, () => {
+  console.log(
+    `Server running on port: ${port}!`,
+  );
+});
