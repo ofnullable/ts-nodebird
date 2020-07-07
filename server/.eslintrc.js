a18d57d0a63dad202a4797acf5b1b624c4129e5f
ofnullable <ofnullable@gmail.com>
2020-07-07 23:10:45 +0900

    :heavy_plus_sign: Add eslint, prettier

---
+++ server/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'airbnb-base',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+};
