ce6b2006ac9a0ec6d769695b34c4c343edd64908
ofnullable <ofnullable@gmail.com>
2020-07-13 23:00:36 +0900

    :rotating_light: Add eslint rule for devDependencies

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -31,6 +31,7 @@ module.exports = {
     'no-param-reassign': 0,
     'import/extensions': 0,
     'import/no-unresolved': 0,
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
     'react/react-in-jsx-scope': 0,
     'react/jsx-filename-extension': 0,
     'react/jsx-props-no-spreading': 0,
